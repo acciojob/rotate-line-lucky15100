a9753c47de0edea6f869eff450e403a639153a5b
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-09-15 11:44:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const line = document.getElementById('line');
+let rotationAngle = 0;
+
+function rotateLine() {
+    // Increment the rotation angle by 2 degrees
+    rotationAngle += 2;
+
+    // Apply the rotation to the line using CSS transform property
+    line.style.transform = `rotate(${rotationAngle}deg)`;
+
+    // Request the next animation frame to create a smooth animation loop
+    requestAnimationFrame(rotateLine);
+}
+
+// Start the animation loop
+rotateLine();
